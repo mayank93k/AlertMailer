6cc69b764647eefffeb0607d5747d5dbb3b928d9
mayank93k <mayank93.k@gmail.com>
2025-03-31 17:21:18 +0530

    Added method comments

--- src/main/scala/scala/alert/job/AlertSender.scala
+++ src/main/scala/scala/alert/job/AlertSender.scala
@@ -1,8 +1,15 @@
 package scala.alert.job
 
-import scala.alert.utility.{AlertMessage, EmailAlertParser, UtilityFunction}
+import scala.alert.utility.{AlertMessage, UtilityFunction}
 
 object AlertSender {
+
+  /**
+   * Entry point of the application.
+   * This method sends a test alert message and processes the alert.
+   *
+   * @param args Command-line arguments (not used in this example).
+   */
   def main(args: Array[String]): Unit = {
     AlertMessage.addMessage("Hello! this is the test mail")
     UtilityFunction.AlertProcessor("TEST")
--- src/main/scala/scala/alert/utility/AlertMessage.scala
+++ src/main/scala/scala/alert/utility/AlertMessage.scala
@@ -4,19 +4,32 @@ package scala.alert.utility
 case class AlertMessage(message: List[String])
 
 object AlertMessage {
+  // Using synchronized ListBuffer for thread safety
   private val messages = scala.collection.mutable.ListBuffer[String]()
 
+  /**
+   * Adds a message to the list.
+   *
+   * @param message The message to be added.
+   */
   def addMessage(message: String): Unit = {
     messages += message
   }
 
+  /**
+   * Retrieves all messages as a combined string.
+   *
+   * @return A single string containing all messages.
+   */
   def getCombinedMessage: String = {
     messages.mkString
   }
 
+
+  /**
+   * Clears all stored messages.
+   */
   def clearMessage(): Unit = {
     messages.clear()
   }
-
-
 }
